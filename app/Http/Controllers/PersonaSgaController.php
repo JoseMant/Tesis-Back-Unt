73f95afd685ad4c169a2d78cdb5a29aa680e4b8a
cesarurbina <curbina@unitru.edu.pe>
2022-06-23 13:55:14 -0500

    SECOND COMMIT

--- app/Http/Controllers/PersonaSgaController.php
+++ app/Http/Controllers/PersonaSgaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\PersonaSga;
 use App\PersonaSuv;
 use App\Usuario;
@@ -87,36 +88,58 @@ class PersonaSgaController extends Controller
 
     public function DatosAlumno($dni)
     {
-        //$pass=md5(md5($request->password));
-        $personaSga=PersonaSga::select('per_nombres','per_apellidos','per_dni','per_mail','per_celular','per_sexo')
-        // ->join('usuario','persona.idpersona','usuario.idpersona')
-        ->Where('per_dni',$dni)->first();
 
-        if(isset($personaSga)){
-            return response()->json(['status' => '200', 'datos_alumno' => $personaSga], 200);
-            //return response()->json(['status' => '200', 'message' => 'Sesión iniciada correctamente.', 'datos_alumno' => $personaSga], 200);
-        }else{
+        DB::beginTransaction();
+        try {
             //$pass=md5(md5($request->password));
-            $personaSuv=PersonaSuv::select('persona.per_nombres','persona.per_apepaterno','persona.per_apematerno','per_tipo_documento','persona.per_dni','persona.per_carneextranjeria',
-            'persona.per_email','persona.per_celular','persona.per_sexo','alumno.idalumno')
-            ->join('alumno','persona.idpersona','alumno.idpersona')
-            ->Where('persona.per_dni',$dni)->first();
-            if(isset($personaSuv)){
+            $personaSga=PersonaSga::select('per_nombres','per_apellidos','per_dni','per_mail','per_celular','per_sexo','per_login')
+            // ->join('usuario','persona.idpersona','usuario.idpersona')
+            ->Where('per_dni',$dni)->first();
+
+            if(isset($personaSga)){
                 $usuario=new Usuario;
-                $usuario->nro_matricula=$personaSuv->idalumno;
-                $usuario->nombres=$personaSuv->per_nombres;
-                $usuario->apellidos=$personaSuv->per_apepaterno." ".$personaSuv->per_apematerno;
-                $usuario->tipo_doc=$personaSuv->per_tipo_documento;
-                $usuario->nro_doc=$personaSuv->per_dni;
-                $usuario->correo=$personaSuv->per_email;
-                $usuario->celular=$personaSuv->per_celular;
-                $usuario->sexo=$personaSuv->per_sexo;
+                $usuario->nro_matricula=$personaSga->per_login;
+                $usuario->nombres=$personaSga->per_nombres;
+                $usuario->apellidos=$personaSga->per_apellidos;
+                $usuario->tipo_doc=1;
+                $usuario->nro_doc=$personaSga->per_dni;
+                $usuario->correo=$personaSga->per_mail;
+                $usuario->celular=$personaSga->per_celular;
+                $usuario->sexo=$personaSga->per_sexo;;
                 return response()->json(['status' => '200', 'datos_alumno' => $usuario], 200);
-                // return response()->json(['status' => '200', 'message' => 'Sesión iniciada correctamente.', 'datos_alumno' => $personaSuv], 200);
+                //return response()->json(['status' => '200', 'message' => 'Sesión iniciada correctamente.', 'datos_alumno' => $personaSga], 200);
             }else{
-                
-                return null;
+                //$pass=md5(md5($request->password));
+                $personaSuv=PersonaSuv::select('persona.per_nombres','persona.per_apepaterno','persona.per_apematerno','per_tipo_documento','persona.per_dni','persona.per_carneextranjeria',
+                'persona.per_email','persona.per_celular','persona.per_sexo','alumno.idalumno')
+                ->join('alumno','persona.idpersona','alumno.idpersona')
+                ->Where('persona.per_dni',$dni)->first();
+                if(isset($personaSuv)){
+                    $usuario=new Usuario;
+                    $usuario->nro_matricula=$personaSuv->idalumno;
+                    $usuario->nombres=$personaSuv->per_nombres;
+                    $usuario->apellidos=$personaSuv->per_apepaterno." ".$personaSuv->per_apematerno;
+                    $usuario->tipo_doc=$personaSuv->per_tipo_documento;
+                    $usuario->nro_doc=$personaSuv->per_dni;
+                    $usuario->correo=$personaSuv->per_email;
+                    $usuario->celular=$personaSuv->per_celular;
+                    if ($personaSuv->per_sexo==0) {
+                        $usuario->sexo="F";
+                    }else{
+                        $usuario->sexo="M";
+                    }
+                    
+                    return response()->json(['status' => '200', 'datos_alumno' => $usuario], 200);
+                    // return response()->json(['status' => '200', 'message' => 'Sesión iniciada correctamente.', 'datos_alumno' => $personaSuv], 200);
+                }else{
+                    return response()->json([ 'message' => 'Alumno no encontrado.']);
+                }
             }
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json(['status' => '400', 'message' => 'Error!!!'], 400);
+            //return redirect()->route('alumno.show', $resolucion->idResolucion) -> with('error', 'Error al registrar alumno');
         }
+        
     }
 }
