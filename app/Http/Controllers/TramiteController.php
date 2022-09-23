d2cd8fa53377ae00d12bc4f15af46683f8e158b5
unknown <cesarurbina>
2022-09-23 09:16:41 -0500

    publicacion

--- app/Http/Controllers/TramiteController.php
+++ app/Http/Controllers/TramiteController.php
@@ -188,15 +188,15 @@ class TramiteController extends Controller
                 // VERIFICAR A QUÉ UNIDAD PERTENECE EL USUARIO PARA OBTENER ESCUELA/MENCION/PROGRAMA
                 $dependenciaDetalle=null;
                 if ($tramite->idUnidad==1) {
-                    // $personaSuv=PersonaSuv::Where('per_dni',$usuario->nro_documento)->first();
-                    // if ($personaSuv) {
-                    //     $dependenciaDetalle=Escuela::Where('idEscuela',$tramite->idDependencia_detalle)->first();
-                    // }else {
+                    $personaSuv=PersonaSuv::Where('per_dni',$usuario->nro_documento)->first();
+                    if ($personaSuv) {
+                        $dependenciaDetalle=Escuela::Where('idEscuela',$tramite->idDependencia_detalle)->first();
+                    }else {
                         $personaSga=PersonaSga::Where('per_dni',$usuario->nro_documento)->first();
                         if ($personaSga) {
                             $dependenciaDetalle=Escuela::Where('idEscuela',$tramite->idDependencia_detalle)->first();
                         }
-                    // }
+                    }
                 }else if ($tramite->idUnidad==2) {
                     
                 }else if ($tramite->idUnidad==3) {
@@ -618,15 +618,15 @@ class TramiteController extends Controller
             // VERIFICAR A QUÉ UNIDAD PERTENECE EL USUARIO PARA OBTENER ESCUELA/MENCION/PROGRAMA
             $dependenciaDetalle=null;
             if ($tramite->idUnidad==1) {
-                // $personaSuv=PersonaSuv::Where('per_dni',$usuario->nro_documento)->first();
-                // if ($personaSuv) {
-                //     $dependenciaDetalle=Escuela::Where('idEscuela',$tramite->idDependencia_detalle)->first();
-                // }else {
+                $personaSuv=PersonaSuv::Where('per_dni',$usuario->nro_documento)->first();
+                if ($personaSuv) {
+                    $dependenciaDetalle=Escuela::Where('idEscuela',$tramite->idDependencia_detalle)->first();
+                }else {
                     $personaSga=PersonaSga::Where('per_dni',$usuario->nro_documento)->first();
                     if ($personaSga) {
                         $dependenciaDetalle=Escuela::Where('idEscuela',$tramite->idDependencia_detalle)->first();
                     }
-                // }
+                }
             }else if ($tramite->idUnidad==2) {
                 
             }else if ($tramite->idUnidad==3) {
@@ -714,15 +714,15 @@ class TramiteController extends Controller
         // VERIFICAR A QUÉ UNIDAD PERTENECE EL USUARIO PARA OBTENER ESCUELA/MENCION/PROGRAMA
         $dependenciaDetalle=null;
         if ($tramite->idUnidad==1) {
-            // $personaSuv=PersonaSuv::Where('per_dni',$usuario->nro_documento)->first();
-            // if ($personaSuv) {
-            //     $dependenciaDetalle=Escuela::Where('idEscuela',$tramite->idDependencia_detalle)->first();
-            // }else {
+            $personaSuv=PersonaSuv::Where('per_dni',$usuario->nro_documento)->first();
+            if ($personaSuv) {
+                $dependenciaDetalle=Escuela::Where('idEscuela',$tramite->idDependencia_detalle)->first();
+            }else {
                 $personaSga=PersonaSga::Where('per_dni',$usuario->nro_documento)->first();
                 if ($personaSga) {
                     $dependenciaDetalle=Escuela::Where('idEscuela',$tramite->idDependencia_detalle)->first();
                 }
-            // }
+            }
         }else if ($tramite->idUnidad==2) {
             
         }else if ($tramite->idUnidad==3) {
