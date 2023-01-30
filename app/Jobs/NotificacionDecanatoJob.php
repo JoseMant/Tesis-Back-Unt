3319b7f871cd8f1697a8778235dd210208763e19
unknown <cesarurbina>
2023-01-30 14:00:43 -0500

    NotoficacionDecanatoJob

--- app/Jobs/NotificacionDecanatoJob.php
+++ app/Jobs/NotificacionDecanatoJob.php
@@ -36,8 +36,13 @@ class NotificacionDecanatoJob implements ShouldQueue
      */
     public function handle()
     {
-        Mail::to($this->decano->correo)
-        // Mail::to("kmuncibay@unitru.edu.pe")
-        ->send(new \App\Mail\NotificacionDecanatoMail($this->decano,$this->tramite,$this->tipo_tramite,$this->tipo_tramite_unidad));
+        if ($this->decano->correo2==null) {
+            Mail::to($this->decano->correo)
+            ->send(new \App\Mail\NotificacionDecanatoMail($this->decano,$this->tramite,$this->tipo_tramite,$this->tipo_tramite_unidad));
+        }else {
+            Mail::to($this->decano->correo)
+            ->cc($this->decano->correo2)
+            ->send(new \App\Mail\NotificacionDecanatoMail($this->decano,$this->tramite,$this->tipo_tramite,$this->tipo_tramite_unidad));
+        }
     }
 }
