913894345dc0029bff8f4bb8d9f7f7764c72cfc6
Maximo534 <mfernandezm@unitru.edu.pe>
2023-07-06 10:32:17 -0500

    importar Excel

--- app/Http/Controllers/PadronController.php
+++ app/Http/Controllers/PadronController.php
@@ -12,7 +12,7 @@ class PadronController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('jwt', ['except' => ['padron']]);
+        $this->middleware('jwt', ['except' => ['padron','correccion']]);
     }
     // public function padron($idOficio){
     //     DB::beginTransaction();
@@ -29,11 +29,14 @@ class PadronController extends Controller
         DB::beginTransaction();
         try {
             // Verificando que el oficio exista
-            $resolucion=Resolucion::find($idResolucion);
+             $resolucion=Resolucion::find($idResolucion);
+            
             if (!$resolucion) {
                 return response()->json(['status' => '400', 'message' =>"La resolución ingresada no existe"], 400);
             }
-            $descarga=Excel::download(new PadronSuneduExport($idResolucion), 'PADRON_SUNEDU_'.$resolucion->nro_resolucion.'.xlsx');
+
+            $name_resolucion=explode("/", $resolucion->nro_resolucion, 2);
+            $descarga=Excel::download(new PadronSuneduExport($idResolucion), 'PADRON_SUNEDU_'.$name_resolucion[0].'.xlsx');
             return $descarga;
         } catch (\Exception $e) {
             DB::rollback();
@@ -42,13 +45,23 @@ class PadronController extends Controller
     }
 
     public function correccion(Request $request){
-        
-        // return $request->all();
+        DB::beginTransaction();
         try {
+            $file=$request->file;
+            $resolucion=Resolucion::find($request->idResolucion);
+            $name_resolucion=explode("/", $resolucion->nro_resolucion,2);
+            $nombre=$file->getClientOriginalName();
+
+            if ($nombre != 'PADRON_SUNEDU_'.$name_resolucion[0].'.xlsx') {
+                DB::rollback();
+                return response()->json(['status' => '400', 'message' => "Archivo incorrecto, subir archivo: PADRON_SUNEDU_".$name_resolucion[0].'.xlsx'], 400);
+            }
+            
             $importacion = new CorrecionSuneduImport;
             $excel=Excel::import( $importacion, $request->file);
-            return $importacion->getDato();
-            return $importacion->getNumFilas();
+            
+            DB::commit();
+            return response()->json(['status' => '200'], 200);
         } catch (\Exception $e) {
             DB::rollback();
             return response()->json(['status' => '400', 'message' => $e->getMessage()], 400);
--- app/Imports/CorrecionSuneduImport.php
+++ app/Imports/CorrecionSuneduImport.php
@@ -7,6 +7,8 @@ use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Tramite;
 use App\Tramite_Detalle;
+use App\Modalidad_Carpeta;
+use App\Programa_Estudios_Carpeta;
 use App\User;
 
 class Correcionsuneduimport implements ToCollection
@@ -15,6 +17,7 @@ class Correcionsuneduimport implements ToCollection
     private $estado=0;
     private $dato="";
     private $numFilas=0;
+    
     public function getStatus(): int
     {
         return $this->estado;
@@ -36,6 +39,7 @@ class Correcionsuneduimport implements ToCollection
     {
       $this->dato = $dato;
     }
+    
     /**
     * @param Collection $collection
     */
@@ -45,73 +49,31 @@ class Correcionsuneduimport implements ToCollection
         $this->setNumFilas( $numAlumnos_porRegistrar );
         foreach ($collection as $key => $value) {
             if ($key>=1) {
-                // $this->setDato($value[11]);
-                // LÓGICA PARA RECHAZAR REQUISITOS
-                //obtener carnets validados
                 if ($value[0]) {
                     $tramite=Tramite::find($value[0]);
                     $tramite_detalle=Tramite_Detalle::find($tramite->idTramite_detalle);
+                    $modalidades_carpeta=Modalidad_Carpeta::where('idTipo_tramite_unidad',$tramite->idTipo_tramite_unidad)
+                    ->where('estado',1)
+                    ->where('modalidad_sustentacion',$value[62])->get();
+                    foreach ($modalidades_carpeta as $modalidad) {
+                        $this->setDato("hola");
+                        if ($modalidad->acto_academico=$value[22]) {
+                            $tramite_detalle->idModalidad_carpeta=$modalidad->idModalidad_carpeta;
+                            break;
+                        }
+                        
+                    }                    
                     
                     $tramite_detalle->fecha_primera_matricula=$value[11];
                     $tramite_detalle->fecha_ultima_matricula=$value[12];
-
-                    switch ($value[20]) {
-                        case 'CICLO REGULAR':
-                            $idprograma=2;
-                            break;
-                        case 'CONVALIDACIÓN':
-                            $idprograma=3;
-                            break;
-                        case 'COMPLEMENTACIÓN ACADÉMICA':
-                            $idprograma=4;
-                            break;
-                        case 'COMPLEMENTACIÓN PEDAGÓGICA':
-                            $idprograma=5;
-                            break;
-                        case 'PROGRAMA PARA ADULTOS':
-                             $idprograma=6;
-                             break;
-                         case 'OTROS':
-                             $idprograma=7;
-                            break;
-
-                        default:
-                            # code...
-                            break;
-                    }
-                    $tramite_detalle->idPrograma_estudios_carpeta=$idprograma;
-
+                    $modalidad_estudio_carpeta=Programa_Estudios_Carpeta::where('descripcion',$value[20])->first();
+                    $tramite_detalle->idPrograma_estudios_carpeta=$modalidad_estudio_carpeta->idPrograma_estudios_carpeta;
                     $tramite_detalle->nro_creditos_carpeta=$value[21];
                     $tramite_detalle->url_trabajo_carpeta=$value[24];
                     $tramite_detalle->nombre_trabajo_carpeta=$value[25];
                     $tramite_detalle->fecha_inicio_acto_academico=$value[30];
+                    $tramite_detalle->originalidad=$value[32];
                     $tramite_detalle->save();
-                //     // $value[22];
-                //     // $value[26];
-                //     // $value[31];
-                //     // $value[32];
-                //     // $value[40];
-                //     // $value[41];
-                //     // $value[42];
-                //     // $value[43];
-                //     // $value[55];
-                //     // $value[56];
-                //     // $value[57];
-                //     // $value[59];
-                //     // $value[60];
-                //     // $value[61];
-                //     // $value[62];
-                //     $this->setDato( $value[11] );
-                
-                    $usuario=User::find($tramite->idUsuario);
-                    $usuario->sexo=$value[8];
-                    $usuario->tipo_documento=$value[9];
-                    $usuario->nro_documento=$value[10];
-                    $usuario->save();
-
-
-
-    
                 }
             }
         }
