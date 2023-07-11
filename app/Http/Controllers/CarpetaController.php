1b50b1535070559bf0fe75318a32b69ea4c67983
Maximo534 <mfernandezm@unitru.edu.pe>
2023-07-11 11:55:19 -0500

    datosDiplomas

--- app/Http/Controllers/CarpetaController.php
+++ app/Http/Controllers/CarpetaController.php
@@ -14,24 +14,26 @@ use Illuminate\Support\Facades\DB;
 class CarpetaController extends Controller
 {
     public function getDataPersona(Request $request){
-        $tramite=Tramite::select('tramite.idTramite','usuario.nombres','usuario.apellidos','usuario.nro_documento','tramite.sede',
+        $tramite=Tramite::select('tramite.idTramite','usuario.nombres','usuario.apellidos','usuario.nro_documento','tramite.sede','tipo_tramite_unidad.diploma_obtenido',
+        'modalidad_carpeta.acto_academico as modalidadSustentancion','tramite_detalle.nro_libro','tramite_detalle.folio','tramite_detalle.nro_registro','resolucion.nro_resolucion',
         DB::raw("(case 
                     when tramite.idUnidad = 1 then dependencia.denominacion  
                     when tramite.idUnidad = 4 then  (select denominacion from dependencia d where d.idDependencia=dependencia.idDependencia2)
                 end) as facultad"),
-        // DB::raw("(case 
-        //             when tramite.idUnidad = 1 then (select nombre from escuela where idEscuela=tramite.idDependencia_detalle)  
-        //             when tramite.idUnidad = 4 then  (select denominacion from mencion where idMencion=tramite.idDependencia_detalle)
-        //         end) as programa"),
-        'tramite.nro_matricula', 'diploma_carpeta.descripcion as denominacion', 'tramite_detalle.codigo_diploma','cronograma_carpeta.fecha_colacion')
+        'programa.nombre as programa', 'tramite.nro_matricula', 'diploma_carpeta.descripcion as denominacion', 'tramite_detalle.codigo_diploma','cronograma_carpeta.fecha_colacion')
         ->join('usuario','usuario.idUsuario','tramite.idUsuario')
         ->join('dependencia','dependencia.idDependencia','tramite.idDependencia')
+        ->join('programa','programa.idPrograma','tramite.idPrograma')
         ->join('tramite_detalle', 'tramite_detalle.idTramite_detalle', 'tramite.idTramite_detalle')
         ->join('diploma_carpeta', 'tramite_detalle.idDiploma_carpeta', 'diploma_carpeta.idDiploma_carpeta')
         ->join('cronograma_carpeta','cronograma_carpeta.idCronograma_carpeta','tramite_detalle.idCronograma_carpeta')
+        ->join('tipo_tramite_unidad','tipo_tramite_unidad.idTipo_tramite_unidad','tramite_detalle.idTipo_tramite_unidad')
+        ->join('modalidad_carpeta','modalidad_carpeta.idModalidad_carpeta','tramite_detalle.idModalidad_carpeta')
+        ->join('resolucion','resolucion.idResolucion','cronograma_carpeta.idResolucion')
         ->where('tramite.idTramite', $request->id)
         ->first();
 
+        if (!$tramite) return response()->json(['status' => '400', 'message' => "No se encuentra carpeta con ese código de diploma"], 400);
 
         $requisito=Tramite_Requisito::select('tramite_requisito.archivo')
         ->where(function($query){
