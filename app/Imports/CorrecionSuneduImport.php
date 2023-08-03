162bfa6107843dd3682a6c9d30771c1a517005a8
Maximo534 <mfernandezm@unitru.edu.pe>
2023-08-03 13:14:38 -0500

    correccionSunedu

--- app/Imports/CorrecionSuneduImport.php
+++ app/Imports/CorrecionSuneduImport.php
@@ -10,6 +10,7 @@ use App\Tramite_Detalle;
 use App\Modalidad_Carpeta;
 use App\Programa_Estudios_Carpeta;
 use App\User;
+use App\Universidad;
 use Carbon\Carbon;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
 
@@ -47,11 +48,13 @@ class Correcionsuneduimport implements ToCollection
     */
     public function collection(Collection $collection)
     {
+        // $this->setDato("hola"); 
         $numAlumnos_porRegistrar = count($collection);
         $this->setNumFilas( $numAlumnos_porRegistrar );
         foreach ($collection as $key => $value) {
             if ($key>=1) {
                 if ($value[0]) {
+                    
                     $tramite=Tramite::find($value[0]);
                     $tramite_detalle=Tramite_Detalle::find($tramite->idTramite_detalle);
                     $modalidades_carpeta=Modalidad_Carpeta::where('idTipo_tramite_unidad',$tramite->idTipo_tramite_unidad)
@@ -65,13 +68,6 @@ class Correcionsuneduimport implements ToCollection
                         }
                     }
 
-                    // $modalidad_carpeta=Modalidad_Carpeta::where('idTipo_tramite_unidad',$tramite->idTipo_tramite_unidad)
-                    // ->where('nombre_padron', $value[22])
-                    // ->where('modalidad_sustentacion',$value[62])
-                    // ->where('estado',1)
-                    // ->first();
-                    // $tramite_detalle->idModalidad_carpeta=$modalidad_carpeta->idModalidad_carpeta;
-
                     if (is_string($value[11])) $tramite_detalle->fecha_primera_matricula=$value[11];
                     else {
                         $date = Date::excelToDateTimeObject($value[11]);
@@ -84,6 +80,12 @@ class Correcionsuneduimport implements ToCollection
                         $tramite_detalle->fecha_ultima_matricula=date_format($date,"Y-m-d");
                     }
                     
+                    if ($value[17]) {
+                        $universidad=Universidad::where('codigo_sunedu',$value[17])->first();
+                        if ($universidad) {
+                            $tramite_detalle->idUniversidad=$universidad->codigo_sunedu;
+                        }
+                    }
                     $modalidad_estudio_carpeta=Programa_Estudios_Carpeta::where('descripcion',$value[20])->first();
                     $tramite_detalle->idPrograma_estudios_carpeta=$modalidad_estudio_carpeta->idPrograma_estudios_carpeta;
                     $tramite_detalle->nro_creditos_carpeta=$value[21];
